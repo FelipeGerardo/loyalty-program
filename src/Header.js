7a08adbfe3ff96e7f30f3e7e9cc3e7fa5e710ba4
FelipeGerardo <felipe89352@gmail.com>
2024-08-29 21:13:54 -0600

    change color of the header and responsive

--- src/Header.js
+++ src/Header.js
@@ -6,17 +6,31 @@ import './styles.css';
 
 // Estilo personalizado para el AppBar
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  padding: 0,
+  padding: 10,
   margin: 0,
   width: '100%',
   boxSizing: 'border-box',
+  backgroundColor: 'rgba(0, 0, 0, 0.85)',
 }));
 
 function Header() {
   return (
     <StyledAppBar position="static">
       <Toolbar>
-        <Typography variant="h4">Programa de lealtad</Typography>
+        <Typography 
+          variant="h4"
+          sx={{
+            fontSize: {
+              xs: '1.5rem',  // Tamaño para pantallas pequeñas
+              sm: '2rem',    // Tamaño para pantallas medianas
+              md: '2.5rem',  // Tamaño para pantallas grandes
+              lg: '3rem',    // Tamaño para pantallas más grandes
+              xl: '3.5rem'   // Tamaño para pantallas extra grandes
+            }
+          }}
+        >
+          Programa de lealtad
+        </Typography>
       </Toolbar>
     </StyledAppBar>
   );
